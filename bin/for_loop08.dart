120530bc702752c6b5aa1d34dbabf2f8e6393399
Begzod <ebegzod477@gmail.com>
2022-08-22 14:22:57 +0500

    code08

--- bin/for_loop08.dart
+++ bin/for_loop08.dart
@@ -1,4 +1,11 @@
 int func(List<int> numbers, int k, int n) {
+  int sum = 0;
+  for (int i = k; i < n; i++) {
+    if (numbers[i] % 2 != 0) {
+      sum = numbers[i] + sum;
+    }
+  }
+  return sum;
 /*
    Given the list of numbers, return the sum of the odd numbers between k and n in the list
 Args:
@@ -8,7 +15,8 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
 }
 
-void main() {}
+void main() {
+  print(func([1, 2, 3, 4, 5], 0, 4));
+}
