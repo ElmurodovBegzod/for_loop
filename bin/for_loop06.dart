b3d368e84f93640823b4fd01498a5553fefc6811
Begzod <ebegzod477@gmail.com>
2022-08-22 13:31:27 +0500

    code06

--- bin/for_loop06.dart
+++ bin/for_loop06.dart
@@ -1,4 +1,8 @@
 int func(List<int> numbers, int k, int n) {
+  int x = 0;
+  for (int i = k; i < n; i = i + 1) {
+    x += numbers[i];
+  }
 /*
    Given the list of numbers, return the sum of the numbers between k and n in the list
 Args:
@@ -8,7 +12,9 @@ Args:
 Return: 
   Int: answer
 */
-  return 0;
+  return x;
 }
 
-void main() {}
+void main() {
+  print(func([6, 2, 8, 1, 6, 3, 7], 2, 4));
+}
